0dd3d315111558566cbfdab77e4c80e2acfbda67
Anish Patel <apatel010@gmail.com>
2020-12-06 15:47:47 -0800

    conditional rendering for todo list

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -5,11 +5,11 @@ import { TodoListItem } from './TodoListItem';
 export const TodoList: React.FC = () => {
     const {state: { todos }} = useContext(TodosContext);
     
-    return (
+    return todos.length > 0 ? (
         <ul>
             {todos.map( (todo: Todo) => 
                  <TodoListItem todo={todo}/>
             )}
         </ul>
-    )
+    ) : <div>Nothing To Do!</div>
 }
